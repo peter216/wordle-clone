7828ab01d8cb287dd6c594b095eec7846b4f0f97
Peter Rubenstein <peter216@gmail.com>
2023-12-23 16:56:44 -0500

    improved

--- wc.py
+++ wc.py
@@ -6,6 +6,7 @@ def main():
     while not newgame.won:
         nextguess = input("Guess? ")
         if nextguess == "qqqqq":
+            print(newgame.answer)
             break
         guessnum, nexthint = newgame.taketurn(nextguess)
         print(f"{guessnum}: {nexthint}")
@@ -13,20 +14,28 @@ def main():
 class Game():
 
     def __init__(self):
-        with open("WORDS") as IFH:
-            FLW = IFH.readlines()
-        self.answer = random.choice(FLW)
+        with open("word-bank.csv") as IFH:
+            self.wordbank = [w.strip() for w in IFH.readlines()]
+        with open("valid-words.csv") as IFH:
+            self.dictionary = [w.strip() for w in IFH.readlines()]
+        self.answer = random.choice(self.wordbank)
         self.won = False
         self.guesscount = 0
 
     def taketurn(self, guess):
+        ans = self.answer
+        if guess not in self.dictionary:
+            return (self.guesscount, "invalid")
         self.guesscount += 1
+        gset = set(guess)
         hint = ['_'] * 5
-        for lnum, ltr in enumerate(guess):
-            if self.answer[lnum] == ltr:
-                hint[lnum] = 'G'
-            elif ltr in self.answer:
-                hint[lnum] = 'Y'
+        for lnum, ltr in enumerate(ans):
+            if ltr in gset:
+                if guess[lnum] == ltr:
+                    hint[lnum] = 'G'
+                else:    
+                    hint[guess.index(ltr)] = 'Y'
+                gset.remove(ltr)
         if hint == ['G'] * 5:
             self.won = True
         return (self.guesscount, " ".join(hint))
